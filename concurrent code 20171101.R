4dc96b8c00c4240266d1ce259cd859e6f2730b73
Jessie Winitzky-Stephens <jwinitzk@slcc.edu>
2017-11-16 16:04:08 -0700

    20171116 COB

--- concurrent code 20171101.R
+++ concurrent code 20171101.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+install.packages("rmarkdown")
+library(rmarkdown)
 
 concurrent <- read_csv("concurrent_20171101.csv")
 concurrent2 <- read_csv("20171110_concurrent.csv")
@@ -317,4 +319,214 @@ chart_enrollpercent <- percent_table %>%
        subtitle = "The majority of students attended SLCC after graduating high school") +
   geom_text(aes(label = paste0(round(freq*100, 0), "%")), vjust = -1)
   
-#per term
+####model: does taking more classes make attending slcc more likely?####
+mod <- glm(slcc ~ courses, data = enrolled_table, family = binomial(link = "logit"))
+summary(mod)
+
+#Call:
+#  glm(formula = slcc ~ courses, data = enrolled_table)
+
+#Deviance Residuals: 
+#  Min       1Q   Median       3Q      Max  
+#-0.7732   0.2268   0.2273   0.2283   0.2374  
+
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)  0.7737094  0.0059419 130.213   <2e-16 ***
+#  courses     -0.0005056  0.0012756  -0.396    0.692    
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#(Dispersion parameter for gaussian family taken to be 0.1760866)
+
+#Null deviance: 2074.2  on 11780  degrees of freedom
+#Residual deviance: 2074.1  on 11779  degrees of freedom
+#AIC: 12976
+
+#Number of Fisher Scoring iterations: 2
+
+#### add covariates
+ce_summarize <- ce_gr_join %>% group_by(PIDM) %>%
+  summarize(num_courses = max(num_courses), Snum = max(BANNER_ID), gender = max(GENDER), ethnicity = max(ETHNICITY), HS = max(HIGH_SCHOOL),
+            grade_num = mean(grade_num), gradyear = max(HS_GRAD_YEAR), enrollafter = max(ENROLLED_AFTER_HS_GRAD), 
+            enrollafter_num = max(enrollafter_num),age = min(AGE_ON_FIRST_DAY), zip = max(ADDRESS_ZIP), min_term = min(TERM_CODE), max_term = max(TERM_CODE))
+
+mod_cov <- glm(enrollafter_num ~ num_courses + grade_num + age + gradyear + gender + ethnicity, 
+               data = ce_summarize, family = binomial(link = "logit"))
+summary(mod_cov)
+
+#create female variable
+ce_summarize <- ce_summarize %>% mutate(female = ifelse(gender == "Female", 1, 0))
+
+#create hispanic & white variables
+ce_summarize <- ce_summarize %>% mutate(white = ifelse(ethnicity == "White", 1, 0))
+ce_summarize <- ce_summarize %>% mutate(hispanic = ifelse(ethnicity == "Hispanic", 1, 0))
+
+mod_cov2 <- glm(enrollafter_num ~ num_courses + grade_num + age + gradyear + female + hispanic + white,
+                data = ce_summarize, family = binomial(link = "logit"))
+summary(mod_cov2)
+
+#Call:
+#  glm(formula = enrollafter_num ~ num_courses + grade_num + age + 
+#        gradyear + female + hispanic + white, family = binomial(link = "logit"), 
+#      data = ce_summarize)
+#
+#Deviance Residuals: 
+#  Min       1Q   Median       3Q      Max  
+#-2.1651   0.5708   0.6924   0.7481   0.9372  
+#
+#Coefficients:
+#  Estimate Std. Error z value Pr(>|z|)    
+#(Intercept) 222.628233  30.460222   7.309 2.70e-13 ***
+#  num_courses  -0.015503   0.008513  -1.821   0.0686 .  
+#grade_num     0.044371   0.021527   2.061   0.0393 *  
+#  age           0.039905   0.032806   1.216   0.2238    
+#gradyear     -0.110349   0.015127  -7.295 2.99e-13 ***
+#  female       -0.013787   0.045232  -0.305   0.7605    
+#hispanic      0.466620   0.084074   5.550 2.86e-08 ***
+#  white         0.069520   0.067712   1.027   0.3046    
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#(Dispersion parameter for binomial family taken to be 1)
+#
+#Null deviance: 12515  on 11621  degrees of freedom
+#Residual deviance: 12416  on 11614  degrees of freedom
+#(159 observations deleted due to missingness)
+#AIC: 12432
+#
+#Number of Fisher Scoring iterations: 4
+
+#need data viz of % of students who went to slcc at each number of courses
+#Count
+ce_summarize %>% group_by(enrollafter, num_courses) %>%
+  summarize(count = n()) %>%
+  ggplot(aes(enrollafter, count)) +
+  geom_col(fill = "#00A8E1") +
+  facet_wrap(~num_courses) +
+  theme_minimal() +
+  labs(x = "Enrolled at SLCC after taking concurrent?", y = "Count")
+
+#Percent
+percent_table_courses <- ce_summarize %>%
+  group_by(num_courses, enrollafter)%>%
+  summarize(n = n()) %>%
+  mutate(freq = n/sum(n)) 
+
+percent_table_courses %>%
+  ggplot(aes(enrollafter, freq)) +
+  geom_col(fill = "#00A8E1") +
+  facet_wrap(~num_courses) +
+  theme_minimal() +
+  labs(x = "Enrolled at SLCC after taking concurrent?", y = "Percent") +
+  geom_text(aes(label = n), vjust = -1)
+
+#collapse num_courses >15
+ce_summarize <- ce_summarize %>% 
+  mutate(num_courses_coll = ifelse(num_courses > 15, 16, num_courses))
+
+percent_courses_coll <- ce_summarize %>%
+  group_by(num_courses_coll, enrollafter)%>%
+  summarize(n = n(), num_courses = min(num_courses)) %>%
+  mutate(freq = n/sum(n)) 
+
+percent_courses_coll$num_courses <- as.integer(percent_courses_coll$num_courses)
+
+chart_pccourses <- percent_courses_coll %>% 
+  arrange(num_courses) %>%
+  ggplot(aes(enrollafter, freq)) +
+  geom_col(fill = "#00A8E1") +
+  facet_wrap(~num_courses_coll) +
+  theme_minimal() +
+  labs(x = "Enrolled at SLCC after taking concurrent?", y = "Proportion", 
+       title = "Proportion of concurrent students who enrolled at SLCC,\nby total number of concurrent courses taken",
+       subtitle = "Number of courses taken was not associated with likelihood of enrolling, \nthough those who took more than 12 courses were less likely to enroll. \nThis may be because they had completed an Associate's degree by high school graduation.",
+       caption = "*The figure above each bar represents the count of students in that category") +
+  geom_text(aes(label = n), vjust = -.5) +
+  ylim(0, 1)
+
+####joining clearinghouse data####
+rm(cech)
+ce_tojoin <- ce_gr_join %>%
+  group_by(PIDM) %>%
+  summarize(num_ce_courses = max(num_courses), max_ce_term = max(TERM_CODE), HS_GRAD_YEAR = max(HS_GRAD_YEAR), 
+            ce_gpa = mean(grade_num), enrollafter = max(enrollafter_num))
+
+cech <- left_join(ch, ce_tojoin, by = "PIDM")
+
+####which institutions?####
+cech_colleges <- cech %>% group_by(PIDM) %>%
+  summarize(college = max(`College Name`), n = n()) %>%
+  group_by(college) %>%
+  summarize(n = n())
+
+cech_colleges2 <- cech %>% group_by(PIDM, `College Name`) %>% summarize(n()) %>%
+  group_by(`College Name`) %>% summarize(n())
+
+
+####filter out ce enrollments from clearinghouse####
+slcc_cech <- cech %>% filter(`College Name` == "SALT LAKE COMMUNITY COLLEGE")
+slcc_cech_begindate <- slcc_cech %>% group_by(`Enrollment Begin`) %>% summarize(n = n() )
+slcc_cech <- slcc_cech %>% mutate(term_code = ifelse(`Enrollment Begin` > 20120501 & `Enrollment Begin` < 20120731, "201230", 0))
+
+#create term code from begin date
+slcc_cech <- slcc_cech %>% mutate(term_code = ifelse(`Enrollment Begin` %in% 20120501:20120731, 201230,
+                                              ifelse(`Enrollment Begin` %in% 20120801:20121231, 201240,
+                                              ifelse(`Enrollment Begin` %in% 20130101:20130430, 201320,
+                                              ifelse(`Enrollment Begin` %in% 20130501:20130731, 201330,
+                                              ifelse(`Enrollment Begin` %in% 20130801:20131231, 201340,
+                                              ifelse(`Enrollment Begin` %in% 20140101:20140430, 201420,
+                                              ifelse(`Enrollment Begin` %in% 20140501:20140731, 201430,
+                                              ifelse(`Enrollment Begin` %in% 20140801:20141231, 201440,
+                                              ifelse(`Enrollment Begin` %in% 20150101:20150430, 201520,
+                                              ifelse(`Enrollment Begin` %in% 20150501:20150731, 201530,
+                                              ifelse(`Enrollment Begin` %in% 20150801:20151231, 201540,
+                                              ifelse(`Enrollment Begin` %in% 20160101:20160430, 201620,
+                                              ifelse(`Enrollment Begin` %in% 20160501:20160731, 201630,
+                                              ifelse(`Enrollment Begin` %in% 20160801:20161231, 201640,
+                                              ifelse(`Enrollment Begin` %in% 20170101:20170430, 201720,
+                                              ifelse(`Enrollment Begin` %in% 20170501:20170731, 201730,
+                                              ifelse(`Enrollment Begin` %in% 20170801:20171231, 201740,
+                                                     NA))))))))))))))))))
+slcc_cech %>% group_by(term_code) %>% summarize(n = n())
+
+#concurrent student = if max ce term code is >= term code
+slcc_cech_no_ce <- slcc_cech %>%
+  filter(term_code > max_ce_term)
+
+#concurrent enrollment rows found in cech dataset
+slcc_cech_only_ce <- slcc_cech %>%
+  filter(term_code <= max_ce_term)
+
+slcc_cech_only_ce %>% summarise(n())
+slcc_cech_only_ce <- slcc_cech_only_ce %>% rename(term_code = TERM_CODE)
+
+#anti join to slcc_cech
+#this dataset contains every college semester for every student who attended slcc
+slcc_cech_noce <- anti_join(slcc_cech, slcc_cech_only_ce, by = c("PIDM", "term_code"))
+
+#anti join slcc_noce to cech
+#first create term code variable for cech
+cech <- cech %>% mutate(term_code = ifelse(`Enrollment Begin` %in% 20120501:20120731, 201230,
+                                              ifelse(`Enrollment Begin` %in% 20120801:20121231, 201240,
+                                              ifelse(`Enrollment Begin` %in% 20130101:20130430, 201320,
+                                              ifelse(`Enrollment Begin` %in% 20130501:20130731, 201330,
+                                              ifelse(`Enrollment Begin` %in% 20130801:20131231, 201340,
+                                              ifelse(`Enrollment Begin` %in% 20140101:20140430, 201420,
+                                              ifelse(`Enrollment Begin` %in% 20140501:20140731, 201430,
+                                              ifelse(`Enrollment Begin` %in% 20140801:20141231, 201440,
+                                              ifelse(`Enrollment Begin` %in% 20150101:20150430, 201520,
+                                              ifelse(`Enrollment Begin` %in% 20150501:20150731, 201530,
+                                              ifelse(`Enrollment Begin` %in% 20150801:20151231, 201540,
+                                              ifelse(`Enrollment Begin` %in% 20160101:20160430, 201620,
+                                              ifelse(`Enrollment Begin` %in% 20160501:20160731, 201630,
+                                              ifelse(`Enrollment Begin` %in% 20160801:20161231, 201640,
+                                              ifelse(`Enrollment Begin` %in% 20170101:20170430, 201720,
+                                              ifelse(`Enrollment Begin` %in% 20170501:20170731, 201730,
+                                              ifelse(`Enrollment Begin` %in% 20170801:20171231, 201740,
+                                                     NA))))))))))))))))))
+#then anti join slcc noce to cech
+cech_noce <- anti_join(cech, slcc_cech_only_ce, by = c("PIDM", "term_code"))
+
+cech_noce_colleges <- cech_noce %>% group_by(PIDM, `College Name`) %>% summarize(n()) %>%
+  group_by(`College Name`) %>% summarize(count = n())
